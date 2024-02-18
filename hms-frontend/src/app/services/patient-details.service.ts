10c414a76444d0f41a513b3fba897c005a742ac4
shehriar <shehriar1burney@gmail.com>
2024-02-18 20:15:41 +0500

    Added error checking on signup page

--- hms-frontend/src/app/services/patient-details.service.ts
+++ hms-frontend/src/app/services/patient-details.service.ts
@@ -1,16 +1,25 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { PatientDetails } from '../interfaces/patient-details';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PatientDetailsService {
-
+  private patientDetailsSubject = new BehaviorSubject<PatientDetails | null>(null);
+  public patientDetails = this.patientDetailsSubject.asObservable();
   constructor(private http : HttpClient) {}
 
   submitPatientDetails(patientDetails : PatientDetails){
     return this.http.post('http://localhost:3000/api/patients', patientDetails);
+  }
+
+  setSelectedVehicle(patient : PatientDetails) {
+    this.patientDetailsSubject.next(patient);
+  }
+
+  getSelectedVehicle(){
+    return this.patientDetails;
   }
 }
--- hms-frontend/src/app/signup/signup.component.ts
+++ hms-frontend/src/app/signup/signup.component.ts
@@ -13,6 +13,14 @@ export class SignupComponent {
   day : any;
   month : any;
   year : any;
+
+  // True if no error.
+  nameError = true;
+  emailError = true;
+  dobError = true;
+  passwordError = true;
+  phoneError = true;
+
   constructor(private router:Router, private patientService : PatientDetailsService){
     this.patientDetails = {
       name: '',
@@ -28,15 +36,18 @@ export class SignupComponent {
   }
 
   onSubmit(){
-    this.populateUserDetails();
-    this.patientService.submitPatientDetails(this.patientDetails).subscribe({
-      next: (response) => {
-        console.log(response);
-      },
-      error: (error) => {
-        console.error(error);
-      }
-    });
+    if(this.errorChecking()){
+      this.populateUserDetails();
+      this.patientService.submitPatientDetails(this.patientDetails).subscribe({
+        next: (response) => {
+          console.log(response)
+          this.patientService.setSelectedVehicle(this.patientDetails);
+        },
+        error: (error) => {
+          console.error(error);
+        }
+      });
+    }
   }
 
   populateUserDetails(){
@@ -60,5 +71,82 @@ export class SignupComponent {
 
     this.patientDetails.password = (document.getElementById("password") as HTMLInputElement).value;
     this.patientDetails.phone = (document.getElementById("phone") as HTMLInputElement).value;
+  }
+
+  errorChecking() : boolean{
+    this.nameError = this.errorCheckName();
+    this.emailError = this.errorCheckEmail();
+    this.dobError = this.errorCheckDOB();
+    this.passwordError = this.errorCheckPassword();
+    this.phoneError = this.errorCheckPhone();
+
+    if(this.nameError && this.emailError && this.dobError && this.passwordError && this.phoneError){
+      return true;
+    }
+    return false;
+  }
+
+  errorCheckName() : boolean{
+    let name = (document.getElementById("fullname") as HTMLInputElement).value;
+    for(let i = 0; i<name.length; i++){
+      if(name[i] == ' '){
+        return true;
+      }
+    }
+    return false;
+  }
+
+  errorCheckEmail() : boolean{
+    let email = (document.getElementById("email") as HTMLInputElement).value;
+    for(let i = 0; i<email.length; i++){
+      if(email[i] == '@'){
+        for(let j = i; j<email.length; j++){
+          if(email[j] == '.'){
+            return true;
+          }
+        }
+      }
+    }
+    return false;
+  }
+
+  errorCheckDOB() : boolean{
+    let day = (document.getElementById("day") as HTMLInputElement).value;
+    let month = (document.getElementById("month") as HTMLInputElement).value;
+    let year = (document.getElementById("year") as HTMLInputElement).value;
+    if(day == "" || month == "" || year == ""){
+      return false;
+    }
+    let dayInt = parseInt(day, 10);
+    let monthInt = parseInt(month, 10);
+    let yearInt = parseInt(year, 10);
+
+    if(dayInt < 1 || dayInt > 31){
+      return false;
+    }
+    if(monthInt < 1 || monthInt > 12){
+      return false;
+    }
+    if(yearInt < 1900 || yearInt > 2024){
+      return false;
+    }
+    return true;
+  }
+
+  errorCheckPassword() : boolean{
+    let password = (document.getElementById("password") as HTMLInputElement).value;
+    let confirmPassword = (document.getElementById("cpassword") as HTMLInputElement).value;
+    if(password != confirmPassword || password.length == 0){
+      return false;
+    }
+    return true;
+  }
+
+  errorCheckPhone() : boolean{
+    let phone = (document.getElementById("phone") as HTMLInputElement).value;
+    if(phone.length < 9 || phone.length > 11){
+      return false;
+    }
+    return true;
   }
 }
